1a0a38c4666da7048f0635b487d48763633caeed
Usman <usman@Usmans-MacBook-Air.local>
2025-05-15 13:54:33 +0500

    Update base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wordle-4-letter/',
+  base: '/Wordle-4-Letter/',
 })
